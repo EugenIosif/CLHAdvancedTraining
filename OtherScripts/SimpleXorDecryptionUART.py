5d41ed6bdf6708744dfcc98b50a7a2c0f5ee5ec7
EugenIosif <iosifeugen@ymail.com>
2026-04-15 18:46:08 +0300

    test push

---
+++ OtherScripts/SimpleXorDecryptionUART.py
@@ -0,0 +1,17 @@
+import serial
+import time
+import random
+
+ser = serial.Serial(
+    port='COM9',
+    baudrate=115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS
+)
+
+        if(ser.in_waiting > 0):
+            bs = ser.read(ser.in_waiting)  # Clear the buffer
+            print("Data received:", bs)  # Read and print the received data
+            decrypted_data = bytes([b ^ 0xCD for b in bs])  # Decrypt using XOR with 0xCD
+            print("Decrypted data:", decrypted_data)  # Print the decrypted data
