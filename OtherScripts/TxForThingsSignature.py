c30880279c3c6f80df9fc187ab825c35cb0ee2a8
EugenIosif <iosifeugen@ymail.com>
2026-04-29 17:01:20 +0300

    adding the implementation for the board

---
+++ OtherScripts/TxForThingsSignature.py
@@ -0,0 +1,16 @@
+import serial
+import time
+import random
+
+ser = serial.Serial(
+    port='COM9',
+    baudrate=115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS
+)
+while True:
+    if(ser.in_waiting > 0):
+        bs = ser.read(ser.in_waiting)  # Clear the buffer
+        print("Data received:", bs)  # Read and print the received data
+    time.sleep(0.05)
---
+++ TestProject/Debug/Core/Src/memController.d
@@ -0,0 +1,4 @@
+Core/Src/memController.o: ../Core/Src/memController.c \
+ ../Core/Inc/memController.h ../Core/Inc/aes.h
+../Core/Inc/memController.h:
+../Core/Inc/aes.h:
