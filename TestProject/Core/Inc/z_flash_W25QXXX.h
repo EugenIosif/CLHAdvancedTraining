6495e720e6c87155a1a1057dc4042d86e827819b
Eugen Iosif <iosifeugen@ymail.com>
2026-06-05 15:04:59 +0300

    adding the power-up sequence to the exported functions list

--- TestProject/Core/Inc/z_flash_W25QXXX.h
+++ TestProject/Core/Inc/z_flash_W25QXXX.h
@@ -203,6 +203,7 @@ void 	 Flash_BErase32k(uint32_t addr);
 void 	 Flash_BErase64k(uint32_t addr);
 void 	 Flash_ChipErase();
 void 	 Flash_PowerDown();
+void 	 Flash_PowerUp();
 uint8_t  Flash_ReadDevID();
 uint16_t Flash_ReadManufactutrerAndDevID();
 uint32_t Flash_ReadJedecID();
